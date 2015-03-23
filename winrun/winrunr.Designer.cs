f0158235c0cc9a48501edcd9676bfa52d4691a83
tridays <tridays@qq.com>
2015-03-24 00:02:38 +0800

    读写注册表方式补完中，目前遇到了权限问题。

---
+++ winrun/winrunr.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace winrun
+{
+    partial class winrunr
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.lvwApps = new System.Windows.Forms.ListView();
+            this.imglstApps = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // btnReload
+            // 
+            this.btnReload.Location = new System.Drawing.Point(12, 12);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(117, 39);
+            this.btnReload.TabIndex = 0;
+            this.btnReload.Text = "重新加载";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // lvwApps
+            // 
+            this.lvwApps.Location = new System.Drawing.Point(12, 57);
+            this.lvwApps.Name = "lvwApps";
+            this.lvwApps.Size = new System.Drawing.Size(235, 177);
+            this.lvwApps.TabIndex = 1;
+            this.lvwApps.UseCompatibleStateImageBehavior = false;
+            // 
+            // imglstApps
+            // 
+            this.imglstApps.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imglstApps.ImageSize = new System.Drawing.Size(16, 16);
+            this.imglstApps.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // winrunr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 261);
+            this.Controls.Add(this.lvwApps);
+            this.Controls.Add(this.btnReload);
+            this.Name = "winrunr";
+            this.Text = "winrun - 注册表模式";
+            this.Load += new System.EventHandler(this.winrunr_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.ListView lvwApps;
+        private System.Windows.Forms.ImageList imglstApps;
+    }
+}
+
---
+++ winrun/winrunr.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Permissions;
+using Microsoft.Win32;
+
+namespace winrun
+{
+    [RegistryPermissionAttribute(SecurityAction.PermitOnly, Read = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths")]
+    [FileIOPermissionAttribute(SecurityAction.PermitOnly, AllFiles = FileIOPermissionAccess.AllAccess)]
+    public partial class winrunr : Form
+    {
+        RegistryKey regApps = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths");
+        public winrunr()
+        {
+            InitializeComponent();
+        }
+
+        private void winrunr_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            string[] strApps = regApps.GetSubKeyNames();
+            foreach (string s in strApps)
+            {
+                RegistryKey regApp = regApps.OpenSubKey(s);
+                int imgIndex;
+
+                    Icon icon = System.Drawing.Icon.ExtractAssociatedIcon(regApp.GetValue("").ToString());
+                    imglstApps.Images.Add(icon);
+                    imgIndex = imglstApps.Images.Count;
+
+                lvwApps.Items.Add(s, imgIndex);
+            }
+        }
+    }
+}
